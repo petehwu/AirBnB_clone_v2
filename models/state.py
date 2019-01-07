311a30ae991d8b9d151426a3ade9035f90feadc7
dspham <dsvpham@gmail.com>
2019-01-07 02:00:38 +0000

    update state and city attributes

--- models/state.py
+++ models/state.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    # name = ""
+
+    __tablename__ = "states"
+    
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref=backref("state", cascade="all, delete-orphan"))
+
+    @property
+    def cities(self, cls=None):
+        return self.cities
