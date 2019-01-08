4b5a97daaf943c716f257c65693eb7b74218ddb6
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-08 12:40:56 -0800

    fixed pep8 issue

--- models/place.py
+++ models/place.py
@@ -21,13 +21,17 @@ class Place(BaseModel, Base):
         amenity_ids: list of Amenity ids
     """
     __tablename__ = "places"
-    city_id = Column("city_id", String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column("user_id", String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column("city_id", String(60), ForeignKey("cities.id"),
+                     nullable=False)
+    user_id = Column("user_id", String(60), ForeignKey("users.id"),
+                     nullable=False)
     name = Column("name", String(128), nullable=False)
     description = Column("description", String(1024), nullable=True)
     number_rooms = Column("number_rooms", Integer, default=0, nullable=False)
-    number_bathrooms = Column("number_bathrooms", Integer, default=0,  nullable=False)
+    number_bathrooms = Column("number_bathrooms", Integer, default=0,
+                              nullable=False)
     max_guest = Column("max_guest", Integer, default=0,  nullable=False)
-    price_by_night = Column("price_by_night", Integer, default=0, nullable=False)
+    price_by_night = Column("price_by_night", Integer, default=0,
+                            nullable=False)
     latitude = Column("latitude", Float, nullable=True)
     longitude = Column("longitude", Float, nullable=True)
