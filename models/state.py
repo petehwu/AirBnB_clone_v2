25d9f92abb6e44f8cf21b6d59c610292b2ba4969
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-07 17:31:39 -0800

    updated base_model, city, state

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from os import getenv
-
+import models
 
 
 class State(BaseModel, Base):
@@ -15,30 +15,20 @@ class State(BaseModel, Base):
     """
 
     # name = ""
-
-    __tablename__ = "states"
-    
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="State", cascade="all, delete, delete-orphan")
-
-    #TODO:
-    #Link state and city together with getter attibute
-    #FileStorage Attribute between State and City
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="State",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
     @property
     def cities(self):
         s_id = self.id
-        type = self.__class__.__name__
-        lookup = type + "." + s_id
-        print("lookup_key:", lookup)
+        objects = models.storage.all()
         c = []
-        for k, v in FileStorage.__objects:
-            if v["__class__"]== "City" and v["state_id"] == s_id:
+        for k, v in objects.items():
+            if (v.__class__.__name__ == "City" and v.state_id == s_id):
                 c.append(v)
         return(c)
-         # my_dict = dict(FileStorage.__objects)
-        # for k, v in my_dict.items():
-        #     if v == obj:
-        #         del FileStorage.__objects[k]
-        #     self.save()
-        
