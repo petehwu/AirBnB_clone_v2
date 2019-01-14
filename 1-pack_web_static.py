424e0d563f20604a74898462c407b5284a9d6b15
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-14 12:29:28 -0800

    fixed pep8 issue

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from fabric.api import run, local
 from datetime import datetime
+
+
 def do_pack():
     dt = datetime.now()
     tgt = "versions/web_static_" + str(dt.year) + '{:02d}'.format(dt.month) +\
