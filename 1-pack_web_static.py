0e5c54fed87ae589e82287f6089a75699f1a7f43
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-14 12:27:38 -0800

    script to tar a file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from fabric.api import run, local
+from datetime import datetime
+def do_pack():
+    dt = datetime.now()
+    tgt = "versions/web_static_" + str(dt.year) + '{:02d}'.format(dt.month) +\
+          '{:02d}'.format(dt.day) + '{:02d}'.format(dt.hour) +\
+          '{:02d}'.format(dt.minute) + '{:02d}'.format(dt.second) + ".tgz"
+    src = "web_static"
+    local("mkdir -p versions")
+    res = local("tar -cvzf {} {}".format(tgt, src), capture=True)
+    if res.failed:
+        return None
+    else:
+        return tgt
