b39cd65d43585cc7a2a2802a1a56a043bd86c50b
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-14 22:06:04 -0800

    fixed the code to add env back

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 from fabric.api import run, local, env, put, sudo
 from datetime import datetime
 from os import path
-
+env.hosts = ["35.237.46.15", "34.73.216.247"]
 # env.user = 'ubuntu'
 # env.key_filename = "~/.ssh/macbook_id_rsa"
 
@@ -25,25 +25,25 @@ def do_pack():
 
 def do_deploy(archive_path):
     """This function deploys packed code to the servers"""
-    env.hosts = [ "35.237.46.15", "34.73.216.247"]
-    if not isfile(archive_path):
+    if not path.isfile(archive_path):
         return False
     try:
         fonly = archive_path[archive_path.find("/"):]
         ffolder = fonly[:fonly.find(".")]
         dest_path = "/data/web_static/releases"
-        result = put(archive_path, "/tmp" + fonly)
-        result = sudo("rm -rf " + dest_path + ffolder + "/")
-        result = sudo("mkdir -p " + dest_path + ffolder + "/")
-        result = sudo("tar -xzf /tmp" + fonly + " -C " +
-                     dest_path + ffolder + "/")
-        result = sudo("rm /tmp" + fonly)
-        result = sudo("mv " + dest_path + ffolder + "/web_static/* " +
-                     dest_path + ffolder + "/")
-        result = sudo("rm -rf " + dest_path + ffolder + "/web_static")
-        result = sudo("rm -rf /data/web_static/current")
-        result = sudo("ln -s " + dest_path + ffolder + "/ " +
-                     "/data/web_static/current")
+        put(archive_path, "/tmp" + fonly)
+        sudo("rm -rf " + dest_path + ffolder + "/")
+        sudo("mkdir -p " + dest_path + ffolder + "/")
+        sudo("tar -xzf /tmp" + fonly + " -C " +
+             dest_path + ffolder + "/")
+        sudo("rm /tmp" + fonly)
+        sudo("mv " + dest_path + ffolder + "/web_static/* " +
+             dest_path + ffolder + "/")
+        sudo("rm -rf " + dest_path + ffolder + "/web_static")
+        sudo("rm -rf " + dest_path + "/test")
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s " + dest_path + ffolder + "/ " +
+             "/data/web_static/current")
         print("New version deployed!")
         return True
     except:
